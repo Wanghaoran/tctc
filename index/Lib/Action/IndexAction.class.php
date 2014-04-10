ef65b8c35ac2d45d28b68f1598e0bc558e3e7645
WangHaoRan <whrlmc@139.com>
2014-04-11 00:19:10 +0800

    制作QQ登录

--- index/Lib/Action/IndexAction.class.php
+++ index/Lib/Action/IndexAction.class.php
@@ -101,7 +101,81 @@ class IndexAction extends Action {
         }
     }
 
+    public function tencentlogin(){
+        //申请到的appid
+        $_SESSION["appid"]    = '101058925';
+        //申请到的appkey
+        $_SESSION["appkey"]   = "938492aa272936763f83622c19fbe64f";
+        //QQ登录成功后跳转的地址,请确保地址真实可用，否则会导致登录失败。
+        $_SESSION["callback"] = "http://1000kmpacificrav4.tctc.com.cn/Index/tencentcheck";
+        //QQ授权api接口.按需调用
+        $_SESSION["scope"] = "get_user_info";
+
+        $_SESSION['state'] = md5(uniqid(rand(), TRUE)); //CSRF protection
+        $login_url = "https://graph.qq.com/oauth2.0/authorize?response_type=code&client_id="
+            . $_SESSION["appid"] . "&redirect_uri=" . urlencode($_SESSION["callback"])
+            . "&state=" . $_SESSION['state']
+            . "&scope=".$_SESSION["scope"] ;
+        header("Location:$login_url");
+
+    }
+
     public function tencentcheck(){
+
+        //回调验证
+        if($_REQUEST['state'] == $_SESSION['state']) //csrf
+        {
+            $token_url = "https://graph.qq.com/oauth2.0/token?grant_type=authorization_code&"
+                . "client_id=" . $_SESSION["appid"]. "&redirect_uri=" . urlencode($_SESSION["callback"])
+                . "&client_secret=" . $_SESSION["appkey"]. "&code=" . $_REQUEST["code"];
+
+            $response = file_get_contents($token_url);
+            if (strpos($response, "callback") !== false)
+            {
+                $lpos = strpos($response, "(");
+                $rpos = strrpos($response, ")");
+                $response  = substr($response, $lpos + 1, $rpos - $lpos -1);
+                $msg = json_decode($response);
+                if (isset($msg->error))
+                {
+                    echo "<h3>error:</h3>" . $msg->error;
+                    echo "<h3>msg  :</h3>" . $msg->error_description;
+                    exit;
+                }
+            }
+
+            $params = array();
+            parse_str($response, $params);
+            $_SESSION["access_token"] = $params["access_token"];
+
+        }
+        else
+        {
+            die("The state does not match. You may be a victim of CSRF.");
+        }
+
+
+        //获取openID
+        $graph_url = "https://graph.qq.com/oauth2.0/me?access_token=" . $_SESSION['access_token'];
+
+        $str  = file_get_contents($graph_url);
+        if (strpos($str, "callback") !== false)
+        {
+            $lpos = strpos($str, "(");
+            $rpos = strrpos($str, ")");
+            $str  = substr($str, $lpos + 1, $rpos - $lpos -1);
+        }
+
+        $user = json_decode($str);
+        if (isset($user->error))
+        {
+            echo "<h3>error:</h3>" . $user->error;
+            echo "<h3>msg  :</h3>" . $user->error_description;
+            exit;
+        }
+
+        $openid = $user->openid;
+        dump($openid);
 
     }
 
